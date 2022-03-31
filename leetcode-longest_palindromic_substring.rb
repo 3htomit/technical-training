a09f68eade503d9802187437366134dd519cb6e5
TimothÃee Boiteau <timotheeboiteau@gmail.com>
2022-03-31 17:12:58 +0200

    LeetCode - Palindromic substring ex. almost done

---
+++ leetcode-longest_palindromic_substring.rb
@@ -0,0 +1,61 @@
+def longest_palindrome(s)
+  letters = s.split("")
+  palindrome = ""
+
+  letters.each_with_index { |l, i|
+    n = 0
+    l_size = letters[i...(letters.size)].size
+
+    while l_size > 1
+      l_range = letters[i...(letters.size - n)]
+      palindrome = l_range.join if l_range == l_range.reverse && l_range.size > palindrome.length
+
+      n += 1
+      l_size = letters[i...(letters.size - n)].size
+      break if l_size <= palindrome.length
+    end
+  }
+
+  if palindrome.empty?
+    p palindrome = letters.first
+  else
+    p palindrome
+  end
+end
+
+
+## TEST
+
+s_a = "babad"
+puts "\n String A:"
+longest_palindrome(s_a)
+# Output: "bab"
+
+s_b = "cbbd"
+puts "\n String B:"
+longest_palindrome(s_b)
+# Output: "bb"
+
+s_c = "cbbdtepipetsqsrv"
+puts "\n String C:"
+longest_palindrome(s_c)
+# Output: "tepipet"
+
+s_d = "a"
+puts "\n String D:"
+longest_palindrome(s_d)
+# Output: "a"
+
+s_e = "ac"
+puts "\n String E:"
+longest_palindrome(s_e)
+# Output: "a"
+
+s_f = "bb"
+puts "\n String F:"
+longest_palindrome(s_f)
+# Output: "bb"
+
+s_g = "zudfweormatjycujjirzjpyrmaxurectxrtqedmmgergwdvjmjtstdhcihacqnothgttgqfywcpgnuvwglvfiuxteopoyizgehkwuvvkqxbnufkcbodlhdmbqyghkojrgokpwdhtdrwmvdegwycecrgjvuexlguayzcammupgeskrvpthrmwqaqsdcgycdupykppiyhwzwcplivjnnvwhqkkxildtyjltklcokcrgqnnwzzeuqioyahqpuskkpbxhvzvqyhlegmoviogzwuiqahiouhnecjwysmtarjjdjqdrkljawzasriouuiqkcwwqsxifbndjmyprdozhwaoibpqrthpcjphgsfbeqrqqoqiqqdicvybzxhklehzzapbvcyleljawowluqgxxwlrymzojshlwkmzwpixgfjljkmwdtjeabgyrpbqyyykmoaqdambpkyyvukalbrzoyoufjqeftniddsfqnilxlplselqatdgjziphvrbokofvuerpsvqmzakbyzxtxvyanvjpfyvyiivqusfrsufjanmfibgrkwtiuoykiavpbqeyfsuteuxxjiyxvlvgmehycdvxdorpepmsinvmyzeqeiikajopqedyopirmhymozernxzaueljjrhcsofwyddkpnvcvzixdjknikyhzmstvbducjcoyoeoaqruuewclzqqqxzpgykrkygxnmlsrjudoaejxkipkgmcoqtxhelvsizgdwdyjwuumazxfstoaxeqqxoqezakdqjwpkrbldpcbbxexquqrznavcrprnydufsidakvrpuzgfisdxreldbqfizngtrilnbqboxwmwienlkmmiuifrvytukcqcpeqdwwucymgvyrektsnfijdcdoawbcwkkjkqwzffnuqituihjaklvthulmcjrhqcyzvekzqlxgddjoir"
+puts "\n String G:"
+longest_palindrome(s_g)
